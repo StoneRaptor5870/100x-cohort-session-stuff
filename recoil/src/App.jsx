b393e1ac39e397999e5e9a5ae0bfe76c6b965dd1
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-04-30 13:51:14 +0530

    useRecoilState

--- recoil/src/App.jsx
+++ recoil/src/App.jsx
@@ -1,10 +1,51 @@
-function App() {
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
+import { countAtom } from "./store/atoms/count";
 
+function App() {
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
+    </div>
+  );
 }
 
-export default App
+function Count() {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  );
+}
+
+function CountRenderer() {
+  const count = useRecoilValue(countAtom);
+  return <div>{count}</div>;
+}
+
+function Buttons() {
+  const [count, setCount] = useRecoilState(countAtom);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+export default App;
