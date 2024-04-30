f136b72af378b550c8c2e1432432cb1d0e461a78
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-04-30 22:33:57 +0530

    atomFamily

--- recoil-advance/advance-bits/src/App.jsx
+++ recoil-advance/advance-bits/src/App.jsx
@@ -1,12 +1,33 @@
+import { RecoilRoot, useRecoilState } from 'recoil'
 import './App.css'
+import { todosAtomFamily } from './store/atomFamily'
+import PropTypes from 'prop-types';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <RecoilRoot>
+      <Todo id={1}/>
+      <Todo id={2}/>
+      <Todo id={2}/>
+    </RecoilRoot>
   )
 }
+
+function Todo({id}) {
+  const [todo] = useRecoilState(todosAtomFamily(id));
+
+  return (
+    <>
+      {todo.title}
+      {todo.description}
+      <br />
+    </>
+  );
+}
+
+Todo.propTypes = {
+  id: PropTypes.number.isRequired
+};
 
 export default App
