7dec8f5fcf80aa890a595de0ec1fd53a1aeb02cf
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-05-09 15:38:54 +0530

    auth middleware

--- hono-cloudflare/src/index.ts
+++ hono-cloudflare/src/index.ts
@@ -1,9 +1,26 @@
-import { Hono } from 'hono'
+import { Hono, Next, Context } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+async function authMiddleware(c: Context, next: Next) {
+  if (c.req.header("Authorization")) {
+    await next()
+  } else {
+    return c.text("you dont have access");
+  }
+}
+
+app.post('/', authMiddleware, async (c) => {
+  const body = await c.req.json()
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+
   return c.text('Hello Hono!')
+})
+
+app.get('/', async (c) => {
+  return c.text("HELLO THERE")
 })
 
 export default app
