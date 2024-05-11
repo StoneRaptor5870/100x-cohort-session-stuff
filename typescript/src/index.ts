b857998747d279cbe6931d6136b759f1195552b3
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-05-11 23:42:54 +0530

    pick, partial and readonly api usecase

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -9,3 +9,42 @@ function sumOfAges(user1: User, user2: User) {
 
 const age = sumOfAges({name: "tara", age: 28}, {name: "anakin", age: 22});
 console.log(age);
+
+// pick api
+interface UserEx {
+  id: string,
+  name: string,
+  age: number,
+  email: string,
+  password: string
+}
+
+type UpdateProps = Pick<UserEx, "name" | "age" | "email">
+
+// partial api
+type UpdatedPropsOptional = Partial<UpdateProps>
+
+function updatedUser(updatedProps: UpdatedPropsOptional) {
+  // hit the database to update the user
+  
+  Object.keys(updatedProps).forEach((key) => {
+    const value = updatedProps[key as keyof UpdatedPropsOptional];
+    console.log(`${key}: ${value}`);
+  });
+}
+
+updatedUser({name: "nischay", age: 24, email: "abc@gmail.com"});
+updatedUser({name: "anuj", age: 22});
+
+// readonly
+interface Config {
+  readonly endpoint: string;
+  readonly apiKey: string;
+}
+
+const config: Readonly<Config> = {
+  endpoint: 'https://api.example.com',
+  apiKey: 'abcdef123456',
+};
+
+// config.apiKey = 'newkey'; // Error: Cannot assign to 'apiKey' because it is a read-only property.
