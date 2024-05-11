052fbcd100dfe28844933e682e910d09122a92e7
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-05-12 01:17:59 +0530

    type inference in zod

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -36,7 +36,7 @@ function updatedUser(updatedProps: UpdatedPropsOptional) {
 updatedUser({name: "nischay", age: 24, email: "abc@gmail.com"});
 updatedUser({name: "anuj", age: 22});
 
-// readonly
+// readonly api
 interface Config {
   readonly endpoint: string;
   readonly apiKey: string;
@@ -48,3 +48,53 @@ const config: Readonly<Config> = {
 };
 
 // config.apiKey = 'newkey'; // Error: Cannot assign to 'apiKey' because it is a read-only property.
+
+// record api
+// interface User {
+//   id: string;
+//   name: string;
+// }
+
+// type Users = { [key: string]: User };
+
+// const users: Users = {
+//   'abc123': { id: 'abc123', name: 'John Doe' },
+//   'xyz789': { id: 'xyz789', name: 'Jane Doe' },
+// };
+
+interface User2 {
+  id: string;
+  name: string;
+}
+
+type Users = Record<string, User2>;
+
+const users: Users = {
+  'abc123': { id: 'abc123', name: 'John Doe' },
+  'xyz789': { id: 'xyz789', name: 'Jane Doe' },
+};
+
+console.log(users['abc123']);
+
+// map api
+
+// Initialize an empty Map
+const usersMap = new Map<string, User2>();
+
+// Add users to the map using .set
+usersMap.set('abc123', { id: 'abc123', name: 'John Doe' });
+usersMap.set('xyz789', { id: 'xyz789', name: 'Jane Doe' });
+
+// Accessing a value using .get
+console.log(usersMap.get('abc123')); // Output: { id: 'abc123', name: 'John Doe' }
+
+// exclude api
+type EventType = 'click' | 'scroll' | 'mousemove';
+
+type ExcludeEvent = Exclude<EventType, 'scroll'>; // 'click' | 'mousemove'
+
+const handleEvent = (event: ExcludeEvent) => {
+  console.log(`Handling event: ${event}`);
+};
+
+handleEvent('click');
