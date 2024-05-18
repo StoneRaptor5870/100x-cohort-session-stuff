1d300e759adde12971b627d7e7f10e822d92eeb8
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-05-19 02:21:37 +0530

    signin-signup

---
+++ blogging-app/frontend/src/components/Quote.tsx
@@ -0,0 +1,14 @@
+export const Quote = () => {
+  return (
+    <div className="bg-slate-200 h-screen flex justify-center flex-col">
+      <div className="flex justify-center">
+        <div className="max-w-lg">
+          <div className="text-3xl font-bold">
+          “Blogging is to writing what extreme sports are to athletics: more free-form, more accident-prone, less formal, more alive. It is, in many ways, writing out loud."
+          </div>
+          <div className="max-w-md text-xl font-light text-slate-800 mt-4">Andrew Sullivan</div>
+        </div>
+      </div>
+    </div>
+  )
+}
