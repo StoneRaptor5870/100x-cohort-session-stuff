c73413852cba559b433d238a721f37a0b34be436
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-05-22 23:42:03 +0530

    docker compose file

--- docker-network/src/db.ts
+++ docker-network/src/db.ts
@@ -1,6 +1,6 @@
 import mongoose, { Schema, model } from 'mongoose';
 
-const mongoUrl: string = 'mongodb://mongo_nischay:27017/myDatabase';
+const mongoUrl: string = process.env.MONGO_URL || 'mongodb://mongo_nischay:27017/myDatabase';
 
 // Connect to MongoDB
 mongoose.connect(mongoUrl)
--- docker-network/src/index.ts
+++ docker-network/src/index.ts
@@ -1,3 +1,5 @@
+import { config } from 'dotenv';
+config();
 import express, { Express, Request, Response } from 'express';
 import { User } from './db';
 
