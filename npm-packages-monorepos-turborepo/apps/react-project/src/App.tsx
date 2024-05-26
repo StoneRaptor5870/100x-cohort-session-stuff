5c8a6b7b5050f4c04a8f8ff3cf49ab5d86b0e921
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-05-26 17:13:42 +0530

    adding backend app

--- npm-packages-monorepos-turborepo/apps/react-project/src/App.tsx
+++ npm-packages-monorepos-turborepo/apps/react-project/src/App.tsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { Button } from '@repo/ui/button';
+ 
 function App() {
   const [count, setCount] = useState(0)
 
@@ -21,6 +22,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Button appName='react-app'>
+          Hi there
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
