34152aee5d608594d0c13e81ba63cadfebb773f8
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-05-28 02:42:08 +0530

    auth route handler

---
+++ next-auth/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function GET(req: NextRequest, { params: { authRoutes } }: {
+  params: {
+    authRoutes: string[]
+  }
+}) {
+  console.log(authRoutes);
+  return NextResponse.json({
+    message: "Handler",
+  });
+}
