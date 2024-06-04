288e33fdf4878f4670411160981fc290a186d400
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-06-05 00:18:44 +0530

    ws client nextjs

---
+++ web-sockets/ws-client-nextjs/app/page.tsx
@@ -0,0 +1,56 @@
+"use client"
+import { useEffect, useState } from "react";
+
+function Home() {
+  const [socket, setSocket] = useState<null | WebSocket>(null);
+  const [latestMessage, setLatestMessage] = useState("");
+  const [message, setSetMessage] = useState("");
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+    socket.onopen = () => {
+      console.log("connected");
+      setSocket(socket);
+    };
+    socket.onmessage = (message) => {
+      console.log("Received message:", message.data);
+      setLatestMessage(message.data);
+    };
+    return () => {
+      socket.close();
+    };
+  }, []);
+
+  if (!socket) {
+    return <div>Connecting to socket server...</div>;
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        gap: "10px",
+        alignItems: "center",
+        width: "20%",
+      }}
+    >
+      <input className="text-black"
+        onChange={(e) => {
+          setSetMessage(e.target.value);
+        }}
+      ></input>
+      <button
+        onClick={() => {
+          socket.send(message);
+        }}
+      >
+        Send
+      </button>
+      <div>{latestMessage}</div>
+    </div>
+  );
+}
+
+export default Home;
