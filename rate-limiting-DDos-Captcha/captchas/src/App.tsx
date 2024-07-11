0e24da838244964787140414c4691def57a75ae8
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-07-12 02:14:48 +0530

    updated backend for capcha logic, frontend for captcha

--- rate-limiting-DDos-Captcha/captchas/src/App.tsx
+++ rate-limiting-DDos-Captcha/captchas/src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Turnstile } from "@marsidev/react-turnstile";
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [token, setToken] = useState<string>("");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        justifyContent: "center",
+        alignContent: "center",
+        marginTop: "20px"
+      }}
+    >
+      <input placeholder="OTP" style={{ width: "450px" }}></input>
+      <input placeholder="New password" style={{ width: "450px" }}></input>
+
+      <Turnstile
+        onSuccess={(token) => {
+          setToken(token);
+        }}
+        siteKey="0x4AAAAAAAXtEe2JIeAEUcjX" /* dummy siteKey */
+      />
+
+      <button
+        onClick={() => {
+          axios.post("http://localhost:3000/reset-password", {
+            email: "abc@gmail.com",
+            otp: "1234",
+            token: token,
+          });
+        }}
+        style={{ width: "150px", marginTop: "-20px" }}
+      >
+        Update password
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
