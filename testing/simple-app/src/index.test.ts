08dc6e2d3bcf4c56d58068baa6b260ad5e3a6591
StoneRaptor5870 <nischay.dtu@gmail.com>
2024-07-17 22:09:00 +0530

    simple app testing more tests

--- testing/simple-app/src/index.test.ts
+++ testing/simple-app/src/index.test.ts
@@ -1,8 +1,22 @@
-import { describe, expect, test } from '@jest/globals';
-import { sum } from './index';
+import { describe, expect, it } from '@jest/globals';
+import { sum, multiple } from './index';
 
 describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
+  it('adds 1 + 2 to equal 3', () => {
     expect(sum(1,2)).toBe(3);
+  });
+
+  it('adds -1 + -3 to equal -4', () => {
+    expect(sum(-1,-3)).toBe(-4);
+  });
+});
+
+describe('multiple module', () => {
+  it('adds 1 * 2 to equal 2', () => {
+    expect(multiple(1,2)).toBe(2);
+  });
+
+  it('adds -2 * -3 to equal 6', () => {
+    expect(multiple(-2,-3)).toBe(6);
   });
 });
